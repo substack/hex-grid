9bab8097e8daeb3d04bcd57f6c82df4553031a78
James Halliday <mail@substack.net>
2014-08-23 21:38:18 -0700

    passing update test

---
+++ test/update.js
@@ -0,0 +1,40 @@
+var grid = require('../');
+var test = require('tape');
+
+test('update', function (t) {
+    t.plan(11);
+    var hexes = [
+        { width: 45, height: 60, style: {} },
+        { width: 45, height: 60, style: {} },
+        { width: 45, height: 60, style: {} },
+        { width: 45, height: 60, style: {} },
+        { width: 45, height: 60, style: {} },
+        { width: 45, height: 60, style: {} },
+        { width: 45, height: 60, style: {} },
+        { width: 45, height: 60, style: {} },
+        { width: 45, height: 60, style: {} },
+        { width: 45, height: 60, style: {} }
+    ];
+    var res = grid({ width: 45*3+10 }, hexes);
+    var expected = [
+        { x: 45*0, y: 0 },
+        { x: 45*1, y: 0 },
+        { x: 45*2, y: 0 },
+        { x: 45*0+45/2, y: 60*3/4 },
+        { x: 45*1+45/2, y: 60*3/4 },
+        { x: 45*0, y: 60*3/4*2 },
+        { x: 45*1, y: 60*3/4*2 },
+        { x: 45*2, y: 60*3/4*2 },
+        { x: 45*0+45/2, y: 60*3/4*3 },
+        { x: 45*1+45/2, y: 60*3/4*3 }
+    ];
+    t.deepEqual(res, expected);
+    
+    for (var i = 0; i < 10; i++) {
+        t.deepEqual(hexes[i].style, {
+            position: 'absolute',
+            left: expected[i].x,
+            top: expected[i].y
+        });
+    }
+});
